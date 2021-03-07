a00aa747d727a3e0dfccfb2e1c8cbdfe769a8e54
17-34928-2@student.aiub.edu <17-34928-2@student.aiub.edu>
2021-03-07 23:36:32 +0600

    account class done

--- bank_2/account.cs
+++ bank_2/account.cs
@@ -44,7 +44,61 @@ namespace bank_2
         {
             get { return this.accountType; } set { this.accountType = value; }
         }
+        public void AutoAcoountNumberGenarator()
+        {
+            this.accountNumber = next;
+            next++;
+        }
+        public void TransactionIncrement()
+        {
+            this.transactions++;
+        }
+        public int AccountNumber
+        {
+            get { return this.accountNumber; }
+        }
+        virtual public void Withdraw(double amount)
+        {
 
+        }
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                TransactionIncrement();
+
+            }
+        
+            public void Transfer(double amount, Account receiver)
+            {
+                if (amount > 0 && amount <= balance)
+                {
+                    alance -= amount;
+                    receiver.Deposit(amount);
+                    TransactionIncrement();
+
+                }
+                
+                virtual public void ShowAccountInformation()
+                {
+
+            Console.WriteLine("__Account Information__\nAccount Type - {0}\nAccount Name - {1}\nAccount Number - {2}\nBalance - {3}\nAddress - {4}\nDOB - {5}", AccountType, AccountName, AccountNumber, Balance, Address, DateOfBirth);
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
     }
     
 }
