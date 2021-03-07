43b9d314e145f632717326eb1ffa756eb5e5d72a
17-34928-2@student.aiub.edu <17-34928-2@student.aiub.edu>
2021-03-07 23:18:43 +0600

    account class half done

--- bank_2/account.cs
+++ bank_2/account.cs
@@ -16,6 +16,35 @@ namespace bank_2
         private string accountType;
         static int next = 3300;
         private int transactions = 0;
+
+
+        public string Address
+        {
+            get { return this.address; } set { this.address = value; }
+        }
+        public string AccountName
+        {
+            get { return this.accountName; } set { this.accountName = value; }
+        }
+
+        public int Transactions
+        {
+            get { return this.transactions; }
+
+        }
+        public double Balance
+        {
+            get { return this.balance; } set { this.balance = value; }
+        }
+        public string DateOfBirth
+        {
+            get { return this.dateOfBirth; } set { this.dateOfBirth = value; }
+        }
+        public string AccountType
+        {
+            get { return this.accountType; } set { this.accountType = value; }
+        }
+
     }
     
 }
