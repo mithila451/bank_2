3556f504bc3f50b0583043fe29f9dfecec05c516
17-34928-2@student.aiub.edu <17-34928-2@student.aiub.edu>
2021-03-07 23:55:27 +0600

    savings account project start

---
+++ bank_2/Checking account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank_2
+{
+    class Checking_account:account
+    {
+
+        public override void ShowAccountInformation()
+        {
+            base.ShowAccountInformation();
+        }
+        public override void Withdraw(double amount)
+        {
+
+
+        }
+    }
+}
