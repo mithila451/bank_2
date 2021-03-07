2578232a333725a78012191cc026728d20849df6
17-34928-2@student.aiub.edu <17-34928-2@student.aiub.edu>
2021-03-07 23:59:03 +0600

    Bank project started

---
+++ bank_2/Bank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank_2
+{
+    class Bank
+    {
+        private string bankName;
+        private Account[] myBank = new Account[10];
+
+
+    }
+}
