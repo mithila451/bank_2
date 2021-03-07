189c5b509f3fe25f9073f840912ea897155bd62e
17-34928-2@student.aiub.edu <17-34928-2@student.aiub.edu>
2021-03-08 00:10:28 +0600

    Bank class half done

--- bank_2/Bank.cs
+++ bank_2/Bank.cs
@@ -9,8 +9,67 @@ namespace bank_2
     class Bank
     {
         private string bankName;
-        private Account[] myBank = new Account[10];
+        private account[] myBank = new account[10];
+        
+        
+        public string BankName
+        {
+            get { return this.bankName; }  set { this.bankName = value; }
+        }
+        public void AddAccount(account account)
+        {
+            int flag = 0;
+            for (int i = 0; i < myBank.Length; i++)
+            {
+                if (myBank[i] == null)
+                {
+                    myBank[i] = account;
+                    myBank[i].AutoacoountNumberGenarator();
+                    myBank[i].ShowaccountInformation();
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("Sorry account Couldnt Be Created!");
 
+            }
+
+        }
+
+        public void Deleteaccount(int accountNumber)
+        {
+            int flag = 0;
+            for (int i = 0; myBank[i] != null; i++)
+            {
+                if(myBank[i].AccountNumber == accountNumber)
+                {
+                    while (myBank[i + 1] != null && i < myBank.Length)
+                    {
+                        myBank[i] = myBank[i + 1];
+                        i++;
+
+                    }
+                    myBank[i] = null;
+                    Console.WriteLine("account Deleted Successfully!");
+                    flag = 1;
+                    break;
+
+                }
+
+
+
+
+
+
+            }
+
+
+
+
+
+        }
 
     }
 }
